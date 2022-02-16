75611d0b811ce3eff7ffbcafe78e5aefdfc26bba
webala <webala1001@gmail.com>
2022-02-16 07:32:50 +0300

    thumbnail for each slide added

--- js/main.js
+++ js/main.js
@@ -1,18 +1,12 @@
 //Swiper slider
 var swiper = new Swiper(".bg-slider-thumbs", {
     loop: true,
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
   });
   var swiper2 = new Swiper(".bg-slider", {
     loop: true,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+    spaceBetween: 0,
     thumbs: {
       swiper: swiper,
     },
